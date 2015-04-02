d6a59c21337f840f60de20c2c26aa0a630fefeaa
Mike Bell <bellmr@gmail.com>
2015-04-01 21:40:05 -0400

    Added base calendar and calendar dates classes that store service date/time information and exceptions to the regular times, respectively.

---
+++ delaid_GTFS/GTFSCalendar.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace delaid_GTFS
+{
+	public class GTFSCalendar
+	{
+		public GTFSCalendar (string serviceId, bool[] daysOfTheWeek, 
+			DateTime startDate, DateTime endDate)
+		{
+			this.ServiceID = serviceId;
+			this.DaysOfTheWeek = daysOfTheWeek;
+			this.StartDate = startDate;
+			this.EndDate = endDate;
+		}
+
+		public string ServiceID {
+			get;
+			set;
+		}
+
+		public bool[] DaysOfTheWeek {
+			get;
+			set;
+		}
+
+		public DateTime StartDate {
+			get;
+			set;
+		}
+
+		public DateTime EndDate {
+			get;
+			set;
+		}
+
+	}
+}
+
---
+++ delaid_GTFS/GTFSCalendarDates.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace delaid_GTFS
+{
+	public enum CalendarExceptionTypes {Added=1, Removed}
+
+	/// <summary>
+	/// Exceptions for the service IDs in the GTFSCalendar
+	/// </summary>
+	public class GTFSCalendarDates
+	{
+		public GTFSCalendarDates (GTFSCalendar service, DateTime date, 
+			CalendarExceptionTypes exceptionType)
+		{
+			this.Service = service;
+			this.ExceptionDate = date;
+			this.ExceptionType = exceptionType;
+		}
+
+		public GTFSCalendar Service {
+			get;
+			set;
+		}
+
+		public DateTime ExceptionDate {
+			get;
+			set;
+		}
+
+		public CalendarExceptionTypes ExceptionType {
+			get;
+			set;
+		}
+	}
+}
+
